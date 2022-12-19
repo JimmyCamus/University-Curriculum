17caa5f952a3820909b3eb1a9126d9db9980f32d
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-19 15:22:11 -0300

    feat: function to get all the courses corresponding to a career

--- api/career.api.ts
+++ api/career.api.ts
@@ -1,13 +1,17 @@
 import {
   collection,
   CollectionReference,
+  doc,
   DocumentData,
+  getDoc,
   getDocs,
+  query,
   QuerySnapshot,
+  where,
 } from "firebase/firestore";
 import { getDownloadURL, ref } from "firebase/storage";
 import { firestoreReference, storage } from "../lib/config/firebase.config";
-import { Career } from "../lib/types/career.type";
+import { Career, Course } from "../lib/types/career.type";
 
 export const getCareers = async (): Promise<Career[]> => {
   const careerColectionReference: CollectionReference = collection(
@@ -19,16 +23,51 @@ export const getCareers = async (): Promise<Career[]> => {
     careerColectionReference
   );
 
-  const cleanData: Career[] = data.docs.map((doc) => {
+  const cleanData: Career[] = data.docs.map((docSnap) => {
     return {
-      id: doc.id,
-      name: doc.data().name,
+      id: docSnap.id,
+      name: docSnap.data().name,
     };
   });
   return cleanData;
 };
 
 export const getImage = async (name: String) => {
-  const url =  getDownloadURL(ref(storage, `images/${name}.jpg`));
+  const url = getDownloadURL(ref(storage, `images/${name}.jpg`));
   return url;
+};
+
+export const getCareer = async (id: string) => {
+  const docRef = doc(firestoreReference, "careers", id);
+  const docSnap = await getDoc(docRef);
+
+  if (!docSnap.exists()) {
+    console.log(`Document with id ${id} doesn't exist`);
+    return;
+  }
+
+  const career: Career = {
+    id: docSnap.id,
+    name: docSnap.data().name,
+  };
+
+  return career;
+};
+
+export const getCourses = async (id: string) => {
+  const careerDocRef = doc(firestoreReference, "careers", id);
+  const coursesRef = collection(firestoreReference, "courses");
+  const q = query(coursesRef, where("careers", "array-contains", careerDocRef));
+  const querySnapshot = await getDocs(q);
+  const courses: Course[] = querySnapshot.docs.map((docSnap) => ({
+    id: docSnap.id,
+    name: docSnap.data().name,
+    semester: docSnap.data().semester,
+    successors: docSnap.data().successors ?? [],
+    predecessors: docSnap.data().predecessors ?? [],
+    isHover: false,
+    isPredecessor: false,
+    isSuccessor: false,
+  }));
+  return courses;
 };
