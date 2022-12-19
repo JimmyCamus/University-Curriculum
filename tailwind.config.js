94a5af13805c9a5b5565cfb65ac6997f58cb99bb
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-12-19 15:03:16 -0300

    feat: custom theme was added to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      successor: "#B0EBB5",
+      predecessor: "#96CBEB",
+      "neutral-hover": "#D1D1D1",
+      "card-hover": "#FBFBFB",
+      white: "#ffffff",
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light"],
+    themes: [
+      {
+        corporate: {
+          ...require("daisyui/src/colors/themes")["[data-theme=corporate]"],
+          primary: "#23415b",
+        },
+      },
+    ],
   },
 };
